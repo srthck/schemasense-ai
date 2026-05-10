603a65e6d7196d27e1bb0f7c10070e84a4c49da8
Srthck <sarthakkadam.sk07@gmail.com>
2026-05-10 18:43:33 +0530

    Fix production API integration

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: " https://schemasense-backend.onrender.com/api",
+  baseURL: "https://schemasense-backend.onrender.com/api",
 });
 
 export default api;
